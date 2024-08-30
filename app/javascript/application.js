6344939af4301767ea0cad76c69585c10a306dad
immprada <im.mprada@gmail.com>
2024-08-30 17:35:22 -0500

    add stimulus

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,7 @@
 import "@hotwired/turbo-rails"
 import "./custom"
+import { Application } from "@hotwired/stimulus"
+import HelloController from "controllers/hello_controller"
+
+const application = Application.start()
+application.register("hello", HelloController)
