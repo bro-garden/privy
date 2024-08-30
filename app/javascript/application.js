4d54ef51fa74fb11b6610a49a4cfce44783b0e5f
immprada <im.mprada@gmail.com>
2024-08-30 17:42:03 -0500

    add debugging mode, at console on frontend, write Stimulus.debug = tr

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,5 +3,7 @@ import "./custom"
 import { Application } from "@hotwired/stimulus"
 import HelloController from "controllers/hello_controller"
 
-const application = Application.start()
+const application = Application.start();
 application.register("hello", HelloController)
+
+window.Stimulus = application;
