9d8ae4900b68a0f72a0205411406825a0f6bab6b
immprada <im.mprada@gmail.com>
2024-09-02 21:54:02 -0500

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_26_012113) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_03_004316) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_26_012113) do
     t.boolean "read", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "expiration_limit", null: false
+    t.string "expiration_type", null: false
   end
 
 end
