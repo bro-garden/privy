9879828a66e8ba146a123cbeaccdc5e73a02cac2
immprada <im.mprada@gmail.com>
2024-09-04 08:19:02 -0500

    add #show action to MessageController

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,11 +12,23 @@ class MessagesController < ApplicationController
 
   def show
     @message = Message.find(params[:id])
+    return fail_show('Message not found') unless @message
+
+    message_manager = Messages::Manager.new(@message)
+    @content = message_manager.read_content
+    return render :show if @content.present?
+
+    fail_show('Message has expired')
   end
 
   private
 
   def message_params
     params.require(:message).permit(:content, :expiration_limit, :expiration_type)
+  end
+
+  def fail_show(message)
+    flash[:alert] = message
+    redirect_to new_message_path
   end
 end
