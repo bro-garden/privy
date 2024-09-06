8a2f7f328697f0b4f0c4339d8b919cac12ea75ac
immprada <im.mprada@gmail.com>
2024-09-06 14:46:26 -0500

    install action text

---
+++ app/javascript/plain-scripts/simplemde_initializer.js
@@ -0,0 +1,19 @@
+// app/javascript/plain-scripts/simplemde_initializer.js
+document.addEventListener("turbo:load", () => {
+  console.log('Hello from immprada simple');
+  const editorElement = document.getElementById("markdown-editor");
+
+  if (editorElement) {
+    const editor = new SimpleMDE({
+      element: editorElement,
+      spellChecker: false,
+      status: false,
+      toolbar: ["bold", "italic", "heading", "|", "quote", "link", "unordered-list", "ordered-list"],
+    });
+
+    // const hiddenField = document.getElementById("message-content");
+    // editor.codemirror.on("change", () => {
+    //   hiddenField.value = editor.value();
+    // });
+  }
+});
--- db/migrate/20240826012113_create_messages.rb
+++ db/migrate/20240826012113_create_messages.rb
@@ -1,7 +1,6 @@
 class CreateMessages < ActiveRecord::Migration[7.0]
   def change
     create_table :messages do |t|
-      t.text :content, null: false
       t.boolean :read, default: false
 
       t.timestamps
