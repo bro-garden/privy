209f56cfae26857e394a01862383ebccbd58234e
immprada <im.mprada@gmail.com>
2024-09-06 12:03:22 -0500

    deployt to fly

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,9 @@
 import "@hotwired/turbo-rails"
-import "./custom"
 import { Application } from "@hotwired/stimulus"
 import ClipboardController from "controllers/clipboard_controller"
-import ExpirationController from "./controllers/expiration_controller"
+import ExpirationController from "controllers/expiration_controller"
+
+console.log('Hello from immprada');
 
 const application = Application.start();
 application.register("clipboard", ClipboardController)
