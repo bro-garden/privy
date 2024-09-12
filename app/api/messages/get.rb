de359989057cbdaffb6a15791054f5c5ca21536e
immprada <im.mprada@gmail.com>
2024-09-12 16:10:25 -0500

    add Get specs

--- app/api/messages/get.rb
+++ app/api/messages/get.rb
@@ -8,10 +8,12 @@ module Messages
     get ':id', jbuilder: 'messages/show' do
       message = Message.find(params[:id])
       @content = Messages::Reader.new(message).read_message
+    rescue ActiveRecord::RecordNotFound => e
+      return error!(e.message, :not_found)
     rescue Messages::ExpirationTypeError => e
-      return error!(e.message, 422)
+      return error!(e.message, :unprocessable_entity)
     rescue Messages::ExpiredError => e
-      return error!(e.message, 404)
+      return error!(e.message, :unauthorized)
     end
   end
 end
