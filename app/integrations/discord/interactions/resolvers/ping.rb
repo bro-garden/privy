030564c4ffd453fb441c56f3c103184f899c4dfc
immprada <im.mprada@gmail.com>
2024-10-22 10:15:34 -0500

    update Ping resolver by discussion

--- app/integrations/discord/interactions/resolvers/ping.rb
+++ app/integrations/discord/interactions/resolvers/ping.rb
@@ -2,25 +2,18 @@ module Discord
   module Interactions
     module Resolvers
       class Ping < Resolver
-        EXPECTED_GLOBAL_NAME = 'Discord'.freeze
-        PONG_RESPONSE_TYPE = 1
-
-        Response = Struct.new(:type)
-
-        def call
-          @response_status = RESPONSE_STATUS[:UNAUTHORIZED]
-          @body = {}
-
-          return unless valid_signature? && correct_global_name?
-
-          @response_status = RESPONSE_STATUS[:OK]
-          @body = Response.new(PONG_RESPONSE_TYPE)
-        end
+        attr_reader :type
 
         private
 
+        def execute_action
+          raise Integrations::Discord::UnauthorizedRequestError unless correct_global_name?
+
+          @type = Discord::Resources::Interaction::PONG_TYPE
+        end
+
         def correct_global_name?
-          request.user.global_name == EXPECTED_GLOBAL_NAME
+          user.discord?
         end
       end
     end
