67dcaadec520e9748735e2c2c5b0869479c485fd
Miguel Prada <mapra99@gmail.com>
2024-11-08 14:18:53 -0500

    remove classes

--- app/integrations/discord/interactions/resolvers/message.rb
+++ app/integrations/discord/interactions/resolvers/message.rb
@@ -4,33 +4,51 @@ module Discord
       class Message < DiscordEngine::Resolvers::Resolver
         attr_reader :callback, :components, :content, :flags
 
-        EPHEMERAL_MESSAGE_FLAG = 64
+        MESSAGE_CREATED_CONTENT = 'Hey @everyone! here is a new **privy** message'.freeze
+        REVEAL_BUTTON_LABEL = 'Reveal'.freeze
+        REVEAL_MESSAGE_RESOLVER = 'Discord::Interactions::Resolvers::RevealMessage'.freeze
 
         def execute_action
-          return reveal_message if reveal_privy?
+          message = create_message
+          message_component = create_message_component(message)
+          send_message(message_component)
 
-          create_message
+          @callback = DiscordEngine::InteractionCallback.channel_message_with_source
+          @content = 'Done, message created'
+          @flags = DiscordEngine::Message::EPHEMERAL_FLAG
+          @components = []
         end
 
         private
 
         def create_message
-          create_interaction_response
-
-          creator = Discord::PrivyCreator.new(resolver: self, channel_id:, message_params:, guild:)
-          creator.call
+          message_creator = Messages::Creator.new(
+            params: message_params,
+            source: :discord_guild,
+            external_id: guild.id
+          )
+          message_creator.call
+          message_creator.message
         end
 
-        def reveal_message
-          # TODO: reveal_message privy message
+        def create_message_component(message)
+          reveal_button = DiscordEngine::MessageComponents::Button.new(
+            label: REVEAL_BUTTON_LABEL,
+            style: :success,
+            resolver_name: REVEAL_MESSAGE_RESOLVER,
+            data: { message_id: message.id }
+          )
+
+          action_row = DiscordEngine::MessageComponents::ActionRow.new
+          action_row.add(reveal_button)
+          action_row
         end
 
-        def expiration_type
-          option_value('expiration_type')
-        end
-
-        def expiration_limit
-          option_value('expiration_limit')
+        def send_message(message_component)
+          DiscordEngine::Message.new(
+            content: MESSAGE_CREATED_CONTENT,
+            components: [message_component]
+          ).create(channel_id:)
         end
 
         def message_params
@@ -39,17 +57,6 @@ module Discord
             expiration_limit: option_value('expiration_limit'),
             expiration_type: option_value('expiration_type')
           }
-        end
-
-        def create_interaction_response
-          @callback = DiscordEngine::InteractionCallback.channel_message_with_source
-          @flags = EPHEMERAL_MESSAGE_FLAG
-          @content = 'Done!'
-          @components = []
-        end
-
-        def reveal_privy?
-          activated_component == Discord::ComponentBuilder::REVEAL_BUTTON_NAME
         end
       end
     end
