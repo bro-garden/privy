828f901de69873b8506af3871cba92e76cd2908b
immprada <im.mprada@gmail.com>
2025-01-07 13:15:35 -0500

    Unauthorized status notice, return a message with a url to the message at web interface

--- app/integrations/discord/status_notices/unauthorized.rb
+++ app/integrations/discord/status_notices/unauthorized.rb
@@ -1,12 +1,21 @@
 module Discord
   module StatusNotices
     class Unauthorized
-      CONTENT = '⚠️ Could not create message: please make sure the bot has' \
-                     ' permission to send messages on this channel'.freeze
+      include Rails.application.routes.url_helpers
+
+      CONTENT = '⚠️ I couldn\'t share the message here. Please make sure' \
+                     ' I have permission to send messages on this channel.' \
+                     ' In the meantime, [click here](%<message_url>s) to see it.'.freeze
+
+      attr_reader :message
+
+      def initialize(message)
+        @message = message
+      end
 
       def build
         DiscordEngine::Message.new(
-          content: CONTENT,
+          content: format(CONTENT, message_url: message_url(message.id)),
           components: []
         )
       end
