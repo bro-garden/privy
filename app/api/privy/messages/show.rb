ec97c0bbcfb654996f67244c6238999a27dc7627
immprada <im.mprada@gmail.com>
2025-01-08 10:38:59 -0500

    update API: - `POST`response include change :id field by :uuid - `GET` needs a :uiid param, instead of :id

--- app/api/privy/messages/show.rb
+++ app/api/privy/messages/show.rb
@@ -1,13 +1,13 @@
 module Privy
   module Messages
     class Show < Grape::API
-      desc 'Returns a message by id.'
+      desc 'Returns a message by uuid.'
       params do
-        requires :id, type: Integer, desc: 'ID of the message.'
+        requires :uuid, type: String, desc: 'UUID of the message.'
       end
 
-      get ':id', jbuilder: 'messages/show' do
-        message = Message.find(params[:id])
+      get ':uuid', jbuilder: 'messages/show' do
+        message = Message.find_by!(uuid: params[:uuid])
         @content = ::Messages::Reader.new(message).read_message
       rescue ActiveRecord::RecordNotFound => e
         return error!(e.message, :not_found)
